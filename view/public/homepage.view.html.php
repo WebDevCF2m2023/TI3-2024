cd978ab0066a604287880b9e90fa8b0e1dfada9d
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-29 12:51:04 +0200

    Ajout de mon lien js

--- view/public/homepage.view.html.php
+++ view/public/homepage.view.html.php
@@ -62,5 +62,7 @@
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
      integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
      crossorigin=""></script>
+     <!--Mon JS-->
+     <script src="../js/carteJSON.js"></script>
 </body>
 </html>
