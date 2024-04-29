ce398eb3c10bad01ad6dd0ae83997906d2842d97
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-29 10:50:31 +0200

    if (isset(['connect'])), on include la page connect.view

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,1 +1,37 @@
 <?php
+
+//Gère le site pour un visiteur non connecté
+
+//JSON pour l'API
+if (isset($_GET['json'])) {
+    $datas = getAllLocations($db);
+    if (!is_string($datas)) {
+        echo json_encode($datas);
+    }
+    exit();
+}
+
+// Si on essaie de se connecter
+
+if (isset($_GET['connect'])) {
+
+    // si le formulaire a été envoyé
+    if (isset($_POST['username'], $_POST['passwd'])) {
+        // protection du champs qui sera dans la requête
+        $username = htmlspecialchars(strip_tags(trim($_POST['username'])), ENT_QUOTES);
+        // protection pour les espaces vide
+        $userpwd = trim($_POST['passwd']);
+        //tentative de connexion
+        $connect = connectAdministrator($db, $username, $userpwd);
+        //Connexion réussie
+        if ($connect === true) {
+            header("Location: ./");
+            exit();
+        } else {
+            $error = "Login et/ou mot de passe incorrect";
+        }
+    }
+    //Appel de la vue
+    include "../view/public/connect.view.html.php";
+    exit();
+}
