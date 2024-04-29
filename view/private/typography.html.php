3c9d0dcc9bef405319b36a0827fb6963365088dd
sebastien2506 <jestrovic_8@msn.com>
2024-04-29 14:36:07 +0200

    modif ++

--- view/private/typography.html.php
+++ view/private/typography.html.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="utf-8">
-    <title>DarkPan - Bootstrap 5 Admin Template</title>
+    <title>ti3</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
