e2a3b8333f131a6b86d8e7575a39fe5c1818f6a8
Maskeeded <maskeeded@gmail.com>
2024-04-29 09:51:33 +0200

    Add functions to add and delete location by ID in localisationsModel.php

--- model/localisationsModel.php
+++ model/localisationsModel.php
@@ -32,3 +32,24 @@ function updateLocationByID(PDO $db, int $id, string $nom, string $adresse, int 
         return $e->getMessage();
     }
 }
+
+function addLocation(PDO $db, string $nom, string $adresse, int $codepostal, string $ville, int $nb_velos, float $latitude, float $longitude) : int | string{
+    try {
+        $prepare = $db->prepare("INSERT INTO `localisations` (`nom`, `adresse`, `codepostal`, `ville`, `nb_velos`, `latitude`, `longitude`) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $prepare->execute([$nom, $adresse, $codepostal, $ville, $nb_velos, $latitude, $longitude]);
+        return (int) $db->lastInsertId();
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
+
+function deleteLocationByID(PDO $db, int $id) : bool | string{
+    try {
+        $prepare = $db->prepare("DELETE FROM localisations WHERE id = ?");
+        $prepare->execute([$id]);
+        if($prepare->rowCount() < 1) return "Aucune location avec cette ID.";
+        return true;
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
