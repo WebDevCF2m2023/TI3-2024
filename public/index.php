d1083a2dc595c7f1373bfc793c78a01cb38a5828
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-29 09:55:24 +0200

    majIndex

--- public/index.php
+++ public/index.php
@@ -9,3 +9,25 @@ try{
 }catch(Exception $e){
     die($e->getMessage());
 }
+
+require_once "../model/utilisateursModel.php";
+require_once "../model/localisationsModel.php";
+require_once "../controller/privateController.php";
+require_once "../controller/adminControl.php";
+
+if (isset($_GET["page"])) {
+    switch ($_GET["page"]) {
+        case "accueil" :
+            include ("../view/public/homepage.view.html.php");
+            break;
+    
+ 
+    }
+
+}else if (isset($_GET["connect"])) {
+    include ("../view/public/connect.view.html.php");
+
+
+    
+}
+    
