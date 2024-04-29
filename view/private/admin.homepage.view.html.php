c4fcad7ce59e05e0c9fe39cb14887c2ba4e1c955
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-29 14:27:45 +0200

    Mise en page du tableau avec bootstrap

--- view/private/admin.homepage.view.html.php
+++ view/private/admin.homepage.view.html.php
@@ -24,6 +24,73 @@
     </header>
   </div>
   <!--Fin NAVBAR-->
+  <div id="content">
+        <h3 class="text-center mt-5">Administration des datas</h3>
+        <?php
+        // datas est une chaîne de caractère : erreur SQL !
+        if (is_string($datas)) :
+        ?>
+            <h3 id="alert"><?= $datas ?></h3>
+        <?php
+        // Pas encore de `geoloc`
+        elseif (empty($datas)) :
+        ?>
+            <h3 id="comment">Pas encore de lieu.</h3>
+        <?php
+        // Nous avons des lieux
+        else :
+            // on compte le nombre de données 
+            $nb = count($datas);
+        ?>
+            <!-- <h3>Il y a <?= $nb ?> <?= $nb > 1 ? "lieux" : "lieu" ?></h3>-->
+            <section class="container">
+                <div class="row">
+                    <div class="col-auto" id="tableau">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped"  data-toggle="table" data-show-columns="true" data-search="true" data-pagination="true" data-checkbox-header="true">
+                                <thead>
+                                    <tr>
+                                        <th data-checkbox="true"></th>
+                                        <th data-click-to-select="true">Id</th>
+                                        <th class="text-center">Nom</th>
+                                        <th class="text-center">Adresse</th>
+                                        <th class="text-center">Téléphone</th>
+                                        <th class="text-center">Latitude</th>
+                                        <th class="text-center">Longitude</th>
+                                        <th class="text-center">Modifier</th>
+                                        <th class="text-center">Supprimer</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    // tant qu'on a des données
+                                    // var_dump($datas);
+
+                                    foreach ($datas as $data) :
+                                    ?>
+
+                                        <tr>
+                                            <td></td>
+                                            <td><?= $data['id'] ?></td>
+                                            <td><?= $data['nom'] ?></td>
+                                            <td><?= $data['rue']. " ". $data['codepostal']. " Bruxelles" ?></td>
+                                            <td><?= $data['telephone'] ?></td>
+                                            <td><?= $data['latitude'] ?></td>
+                                            <td><?= $data['longitude'] ?></td>
+                                            <td class="text-center"><a href="?update=<?= $data['id'] ?>"><img src="../img/editIcone.png" alt="update" /></a></td>
+                                            <td class="text-center"><a href="?delete=<?= $data['id'] ?>"><img src="../img/deleteIcon.png" alt="delete" /></a></td>
+
+                                        </tr>
+
+                                    <?php endforeach ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        <?php endif ?>
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
