a28123db4f1f040d0e164ae6fd92403f1dae1561
PommeDeuxTerre <lemonniergregory2@gmail.com>
2024-04-29 10:40:48 +0200

    logout admin

--- model/utilisateursModel.php
+++ model/utilisateursModel.php
@@ -16,3 +16,16 @@ function connection_admin(PDO $db, string $username, string $password){
 }
 
 # Déconnexion de l'administrateur
+function logout_admin(){
+    $_SESSION = array();
+
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    session_destroy();
+}
