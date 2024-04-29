d961e7c72e1ea6cce1b1d5136661124d0be28f6a
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-04-29 12:05:22 +0200

    recup, chargement et update sur locModel faits

--- model/localisationsModel.php
+++ model/localisationsModel.php
@@ -1,3 +1,59 @@
 <?php
 
+function findThemAll(PDO $connect) : array|string {
 
+    $sql = "SELECT ALL * FROM `localisations`;";
+
+    try{
+        // Lulusia Kingdom ? There was never such a place to begin with.
+        $query = $connect->query($sql);
+
+        $result = $query->fetchAll();
+        $query->closeCursor();
+        return $result;
+    }catch(Exception $e){
+        return $e->getMessage();
+    }
+
+}
+
+function findJustOne(PDO $db, int $notTevinFault) : string|bool|array {
+
+    $sql = "SELECT * FROM `localisations` WHERE `id` = 1;";
+    $foundIt = $db->prepare($sql);
+    $foundIt->bindParam(1, $notTevinFault, PDO::PARAM_INT);
+    try{
+        $foundIt->execute();
+        if($foundIt->rowCount()===0) return false;
+        $thisIsIt = $foundIt->fetch();
+        return $thisIsIt;
+    }catch(Exception $e){
+        return $e->getMessage();
+    }
+}
+
+function updateJustOne(PDO $db, int $id): string|bool{
+
+    $sql = "UPDATE `localisations` SET `id`='?',`nom`='?',`adresse`='?',`codepostal`='?',`latitude`='?',`longitude`='?' WHERE `id` = 1;";
+    $foundIt = $db->prepare($sql);
+    try{
+        $foundIt->execute([
+            $id,
+            $nom,
+            $adresse,
+            $codepostal,
+            $lat,
+            $lon,
+            $idFromID
+        ]);
+
+        if($foundIt->rowCount()===0) return false;
+
+        return true;
+
+    }catch(Exception $e){
+        return $e->getMessage();
+    }
+}
+
+function createJustOne(PDO $db){}
