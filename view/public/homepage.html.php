ab6c75abb838f8892985142d4ee0d488e039256c
rammroumm2K <ramybrz00@gmail.com>
2024-04-29 16:24:57 +0200

    last commit of the day

--- view/public/homepage.html.php
+++ view/public/homepage.html.php
@@ -8,48 +8,42 @@
     <!-- CSS de Leaflet -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     <!-- ma CSS -->
-    <link rel="stylesheet" href="css/carteJSON.css">
+    <link rel="stylesheet" href="./css/carteJSON.css">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
 
-    <nav class="navbar navbar-expand-sm navbar-light bg-success shadow-sm mb-5">
-        <div class="container">
-            <a class="navbar-brand text-white font-weight-bold" href="#">GeoMapper</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="fas fa-bars"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active text-white" aria-current="page" href="./">Accueil</a>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="?connect">Connéxion</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <nav class="navbar-home">
+
+    <h1>GeoMapper</h1>
+
+    <button><a href="?connect">GGGG</a></button>
+
+
     </nav>
 
 
 
-    <div class="container text-center">
-
+    <div class="titre">
 
         <h1>Les listes de GeoMapper</h1>
+
+    </div>
         <div id="resultats">
-            <div id="carte"></div>
+            <div id="carte" class="elemento1"></div>
+            
             <div id="liste"></div>
-
-
         </div>
+
+
     </div>
     <!-- JS de Leaflet -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-    <!-- monJS -->
+    
     <script src="js/carteJSON.js"></script>
 
 </body>
