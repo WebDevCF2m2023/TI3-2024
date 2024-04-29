93f1e3333884f31fefb7e17849d47c008376c9d0
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-29 09:31:55 +0200

    Mise en place du cf, appel des dépendances

--- public/index.php
+++ public/index.php
@@ -1,1 +1,18 @@
 <?php
+
+//Contrôleur Frontal
+
+//Lancement de la session
+
+session_start();
+
+//Appel des dépendances
+
+//Configuration
+require_once "../config.php";
+
+//Modèles
+require_once "../model/utilisateursModel.php";
+require_once "../model/localisationsModel.php";
+
+//connexion à la db
