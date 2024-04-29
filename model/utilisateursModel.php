73d9e53704cfc43ccdd63da34f23955d8499f28d
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-29 09:49:37 +0200

    Création de la fonction disconnectAdministrator

--- model/utilisateursModel.php
+++ model/utilisateursModel.php
@@ -38,3 +38,33 @@ function connectAdministrator(PDO $connect, string $user, string $password): boo
 
 
 # Déconnexion de l'administrateur
+
+function disconnectAdministrator(): bool
+{
+    // la session est lancée dans le Contrôleur Frontal , sinon décommentez cette ligne
+    // session_start();
+
+    // Détruit toutes les variables de session
+    $_SESSION = [];
+
+    // Si vous voulez détruire complètement la session, effacez également
+    // le cookie de session.
+    // Note : cela détruira la session et pas seulement les données de session !
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params["path"],
+            $params["domain"],
+            $params["secure"],
+            $params["httponly"]
+        );
+    }
+
+    // Finalement, on détruit la session (fichier texte côté serveur)
+    session_destroy();
+
+    return true;
+}
