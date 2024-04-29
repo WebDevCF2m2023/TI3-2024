37f31072d84603904a9e9ffa2134e4bbf411f3ea
erdaserhan <erhan209@gmail.com>
2024-04-29 09:38:40 +0200

    création de dossiers

--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
 <?php
+
+// lancement de la session
+session_start();
+
+// appel de dépendances
+// configuration
+require_once "../config.php";
+// Modèles
+require_once "../model/utilisateursModel.php";
+require_once "../model/localisationsModel.php";
+
+// connexion à la DB 
+try{
+    // instanciation de notre connexion PDO
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=". DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT , DB_LOGIN, DB_PASSWORD, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+
+}catch(Exception $e){
+    die($e->getMessage());
+}
