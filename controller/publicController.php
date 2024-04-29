d4c7459174a2e33ce8c78faae57e3221bb845d53
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-29 09:56:48 +0200

    majPublicControl

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,1 +1,27 @@
 <?php
+
+
+if(isset($_GET['json'])){
+    $datas = getAllOurdatas($db);
+    if(!is_string($datas)){
+        echo json_encode($datas);
+    }
+    exit();
+}
+
+
+    if(isset($_POST['username'],$_POST['passwd'])){
+
+        $username = htmlspecialchars(strip_tags(trim($_POST['username'])),ENT_QUOTES);
+        $userpwd = trim($_POST['passwd']);
+        $connection = connectAdministrator($db,$username,$userpwd);
+
+        if($connection === true){
+            header("Location: ?bienvenue");
+            die();
+        }else{
+            $error = "Login et/ou mot de passe incorrecte(s)";
+        }
+    }
+
+$ourDatas = getAllOurdatas($db);
