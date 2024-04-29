b121695d9314f9e80563890413023dcc11180d4a
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-04-29 13:38:26 +0200

    trying things out but doesn't seem to work

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,1 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Connexion</h1>
+    <nav>
+        <ul>
+            <li><a href="./">Accueil</a></li>
+            <li><a href="?json" target="_blank">API</a> format JSON</li>
+            <li>Connexion</li>
+        </ul>
+    </nav>
+    <div id="content">
+        <h3>Connexion à notre administration</h3>
+        <?php if(isset($error)): ?>
+            <h4 id="alert"><?=$error?></h4>
+        <?php endif ?>    
+        <form action="" method="POST" name="connexion">
+            <input type="text" name="username" placeholder="Votre login" required><br>
+            <input type="password" name="userpsw" placeholder="Votre mot de passe" required><br>
+            <input type="submit" value="connexion">
+        </form>
+    </div>
+</body>
+</html>
