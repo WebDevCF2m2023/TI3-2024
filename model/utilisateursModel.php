473b19ac836ea548237a57e3a61d2577f89218e8
PommeDeuxTerre <lemonniergregory2@gmail.com>
2024-04-29 10:37:07 +0200

    login admin

--- model/utilisateursModel.php
+++ model/utilisateursModel.php
@@ -1,6 +1,18 @@
 <?php
 
-# Connexion de l'administrateur en utilisant password_verify
-
+# administrator connection
+function connection_admin(PDO $db, string $username, string $password){
+    try {
+        $sql = "SELECT `passwd` FROM `localisations` WHERE `username`=?";
+        $prepare = $db->prepare($sql);
+        $prepare->execute([$username]);
+        $user = $prepare->fetch();
+        $prepare->closeCursor();
+        if (!$user || !password_verify($password, $user["passwd"]))return "username ou mot de passe incorrect";
+        $_SESSION["admin"] = true;
+    }catch (Exception $e){
+        return $e->getMessage();
+    }
+}
 
 # Déconnexion de l'administrateur
