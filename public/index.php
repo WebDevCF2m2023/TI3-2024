5b8d398e8835808400d2c945f0426aa8da1a3909
Maskeeded <maskeeded@gmail.com>
2024-04-29 09:25:26 +0200

    added PDO connection

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
 <?php
+// Start the session
+session_start();
+
+// Require the config file
+require_once("../config.php");
+
+$dsn = DB_DRIVER . ":host=" . DB_HOST . ";charset=" . DB_CHARSET . ";port=" . DB_PORT . ";dbname=". DB_NAME;
+
+try {
+    $db = new PDO($dsn, DB_USER, DB_PASS, [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
