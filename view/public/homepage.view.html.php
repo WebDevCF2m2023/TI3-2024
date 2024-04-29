94c74a56034471d3d9bf82051ea0b633c133c4cf
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-29 12:02:14 +0200

    majStyleHomePublic

--- view/public/homepage.view.html.php
+++ view/public/homepage.view.html.php
@@ -8,6 +8,21 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 </head>
 <body>
+<header>
+<div class="navbar">
+<nav>
+<ul>
+<a href="?accueil"><span class="Gnav">G</span><span class="unav">u</span><span class="inav">i</span><span class="gnav">g</span><span class="lnav">l</span><span class="enav">e</span><span class="Mnav">Maps</span></a>
+    <li>
+    <a  href="?accueil">WELCOME</a></li>
+    <li>
+    <a href="?connect">LOGIN</a></li>
+</ul>
+</nav>
+</div>
+</header>
+
+
 <div id="titleHome">
         <h1 id="welcome"><span class="G">G</span><span class="u">u</span><span class="i">i</span><span class="g">g</span><span class="l">l</span><span class="e">e</span></h1>
 </div>
