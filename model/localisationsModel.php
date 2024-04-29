694153f7c071c9ff9b2b073d649e8e5c5f84c0f8
Maskeeded <maskeeded@gmail.com>
2024-04-29 09:48:47 +0200

    Add functions to get and update location by ID in localisationsModel.php

--- model/localisationsModel.php
+++ model/localisationsModel.php
@@ -10,3 +10,25 @@ function getAllLocations(PDO $db) : array | string{
         return $e->getMessage();
     }
 }
+
+function getLocationByID(PDO $db, int $id) : array | string{
+    try {
+        $prepare = $db->prepare("SELECT * FROM localisations WHERE id = ?");
+        $prepare->execute([$id]);
+        if($prepare->rowCount() < 1) return "Aucune location avec cette ID.";
+        return $prepare->fetch();
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
+
+
+function updateLocationByID(PDO $db, int $id, string $nom, string $adresse, int $codepostal, string $ville, int $nb_velos, float $latitude, float $longitude) : bool | string{
+    try {
+        $prepare = $db->prepare("UPDATE localisations SET nom = ?, adresse = ?, codepostal = ?, ville = ?, nb_velos = ?, latitude = ?, longitude = ? WHERE id = ?");
+        $prepare->execute([$nom, $adresse, $codepostal, $ville, $nb_velos, $latitude, $longitude, $id]);
+        return true;
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
