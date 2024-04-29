f9d827d7abe5ad5a4555891d438a6bf6fce8bdf5
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-29 09:33:59 +0200

    Connexion à la db

--- public/index.php
+++ public/index.php
@@ -16,3 +16,10 @@ require_once "../model/utilisateursModel.php";
 require_once "../model/localisationsModel.php";
 
 //connexion à la db
+
+try {
+    // instanciation de notre connexion PDO
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT, DB_LOGIN, DB_PASSWORD, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
