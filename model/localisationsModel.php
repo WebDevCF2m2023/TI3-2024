e8e6fa9c6c765353e5fcca3bdbbd207a6e13a2b3
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-29 09:54:25 +0200

    Fonction getAllLocations qui recupere tous les champs

--- model/localisationsModel.php
+++ model/localisationsModel.php
@@ -1,1 +1,18 @@
 <?php
+
+
+// Fonction qui récupère tous les champs de `localisations`
+function getAllLocations(PDO $connection): array|string
+{
+    $sql = "SELECT * FROM `localisations`";
+    try {
+        $query = $connection->query($sql);
+
+        // Si il n'y a pas de résultat, fetchAll sera un tableau vide
+        $result = $query->fetchAll();
+        $query->closeCursor();
+        return $result;
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
