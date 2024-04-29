c027ebafcbb28d8abc9d12ebbbc942acaa9fa241
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-04-29 10:18:21 +0200

    public controller

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,2 +1,13 @@
 <?php
-require("../view/public/login.html.php");
+
+if(isset($_GET['login'])){
+
+    if(isset($_POST['user'], $_POST['password'])){
+        // userConnect redirige automatiquement
+        $error = userConnect($db, $_POST['user']);
+    }
+
+    require("../view/public/login.html.php");
+}else{
+    require("../view/public/home.html.php");
+}
