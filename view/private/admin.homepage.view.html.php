35b6537b4513683a162ed8875ba7379730beb074
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-29 13:49:27 +0200

    Ajout navbar basique en bootstrap

--- view/private/admin.homepage.view.html.php
+++ view/private/admin.homepage.view.html.php
@@ -12,6 +12,18 @@
     <title>Accueil Admin</title>
 </head>
 <body>
+    <!--NAVBAR-->
+    <div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="./" class="nav-link text-black">Accueil</a></li>
+        <li class="nav-item"><a href="?create" class="nav-link text-black">Ajouter un lieu</a></li>
+        <li class="nav-item"><a href="?disconnect" class="nav-link text-black">Déconnexion</a></li>
+       
+      </ul>
+    </header>
+  </div>
+  <!--Fin NAVBAR-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
