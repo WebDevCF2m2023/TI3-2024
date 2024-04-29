34cc506224ea34871f281300b3a77cf8e3682fbe
PommeDeuxTerre <lemonniergregory2@gmail.com>
2024-04-29 09:48:17 +0200

    get_localisation_by_id

--- model/localisationsModel.php
+++ model/localisationsModel.php
@@ -1,7 +1,29 @@
 <?php
 
-function get_all_localisations(PDO $db):array{
-    $sql = "SELECT * FROM `localisations`";
-    $query = $db->query($sql);
-    return $query->fetchAll(PDO::FETCH_ASSOC);
+function get_all_localisations(PDO $db):array|string{
+    try {
+        $sql = "SELECT * FROM `localisations`";
+        $query = $db->query($sql);
+        $locations = $query->fetchAll(PDO::FETCH_ASSOC);
+        $query->closeCursor();
+        return $locations;
+    }catch (Exception $e){
+        return $e->getMessage();
+    }
+}
+
+function get_localisation_by_id(PDO $db, int $id):array|string{
+    try {
+        $sql = "SELECT * FROM `localisations` WHERE `id`=?";
+        $prepare = $db->prepare($sql);
+        $prepare->execute([$id]);
+        $locations = $prepare->fetchAll(PDO::FETCH_ASSOC);
+        $prepare->closeCursor();
+        if (count($locations)===0){
+            return "Localisation non trouvée";
+        }
+        return $locations[0];
+    }catch (Exception $e){
+        return $e->getMessage();
+    }
 }
