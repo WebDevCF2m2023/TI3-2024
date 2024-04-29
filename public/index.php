b9419550a08fd50e8abbe9289ca6204383f76aaf
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-04-29 09:28:49 +0200

    front controller

--- public/index.php
+++ public/index.php
@@ -1,1 +1,27 @@
 <?php
+session_start();
+
+require_once("../config.php");
+require_once("../models/usersModel.php");
+require_once("../models/locationsModel.php");
+
+
+$dsn = DB_DRIVER . ":host=" . DB_HOST . ";charset=" . DB_CHARSET . ";port=" . DB_PORT . ";dbname=". DB_NAME;
+
+try {
+    $db = new PDO($dsn, DB_USER, DB_PASS, [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
+if(isset($_GET['json'])){
+    require("../controllers/apiController.php");
+}else if(isset($_SESSION['connected'])){
+    require("../controllers/privateController.php");
+}else{
+    require("../controllers/publicController.php");
+}
+
+$db = null;
