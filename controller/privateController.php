92d42786a5cfa465803beeac796d7a48b0de3299
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-04-29 10:26:02 +0200

    home admin

--- controller/privateController.php
+++ controller/privateController.php
@@ -1,1 +1,2 @@
 <?php
+require("../view/private/home.html.php");
---
+++ view/private/home.html.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+     <link rel="stylesheet" href="css/home.css">
+    <title>Carte interactive Admin</title>
+</head>
+<body>
+    <div id="container">
+        <header>
+            <h1>Carte interactive Admin</h1>
+            <h2>Parcours BD à Bruxelles</h2>
+            <a href="/?login"><button>Connexion à l'administration</button></a>
+        </header>    
+        <div id="map"></div>
+        <div id="liste">
+            <h3>Liste des points</h3>
+            <p>Cliquez sur un élément dans la liste ci-dessous pour le siter sur la carte</p>
+            <hr>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+        crossorigin=""></script>
+    <script src="js/home.js"></script>
+</body>
+</html>
