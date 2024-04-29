1f9e1bcc04bffd1e8b8a2ba6ba3722c7038cb36b
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-29 09:42:47 +0200

    majIndex

--- public/index.php
+++ public/index.php
@@ -1,1 +1,11 @@
 <?php
+
+session_start();
+
+require_once "../config.php";
+
+try{
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=". DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT , DB_LOGIN, DB_PASSWORD, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+}catch(Exception $e){
+    die($e->getMessage());
+}
