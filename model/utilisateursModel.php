6f8631be871f6c33d52cdef040580c552da24462
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-04-29 11:04:09 +0200

    creation des fichers PHP et finalisation des fonctions connect et deconnect admin

--- model/utilisateursModel.php
+++ model/utilisateursModel.php
@@ -2,5 +2,57 @@
 
 # Connexion de l'administrateur en utilisant password_verify
 
+function administratorConnecting(PDO $net, string $user, string $psw) : bool|string 
+{
+    $sql="SELECT ALL * FROM `utilisateurs`";
+
+    $Jumbio = $net-prepare($sql);
+
+    try{
+        $Jumbio->execute([$user]);
+
+        /* Dans le cas où l'utilisateur n'existe pas,
+           on quitte sans afficher l'erreur (bonne
+           pratique contre les hackeurs)*/
+        
+        if($jumbio->rowCount()===0) return false;
+
+        //Résulat mit dans tableau assosiatif
+        $qeqa = $jumbio->fetch();
+
+        if(password_verify($psw, $qeqa['userpsw'])){
+
+            $_SESSION = $qeqa;
+
+            unset($_SESSION['userpsw']);
+
+            return true;
+        }
+        // Si le mot de pass est invalide
+        return false;
+
+    }catch(Exception $e){
+        return $e->getMessage();
+    }
+}
+
 
 # Déconnexion de l'administrateur
+
+function administratorDisconnecting(): bool
+{
+
+    $_SESSION = [];
+
+    if(ini_get("session.user_cookies")){
+        $parameters = session_get_cookie_params();
+        setcookie(session_name(), '', time() -42000,
+        $parameters["path"], $parameters ["domain"],
+        $parameters ["secure"], $parameters ["httponly"]);
+    }
+        session_destroy();
+
+    return true;
+}
+
+
--- public/index.php
+++ public/index.php
@@ -1,1 +1,18 @@
 <?php
+
+session_start();
+
+require_once "../config.php";
+require_once "../utilisateursModel.php";
+require_once "../localisationsModel.php";
+
+try{
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=". DB_NAME . ";charset=" . DB_CHARSET .
+     ";port=" . DB_PORT , DB_LOGIN, DB_PASSWORD, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+
+}catch(Exception $e){
+    die($e->getMessage());
+    // No exit here, we just kill.
+}
+
+
