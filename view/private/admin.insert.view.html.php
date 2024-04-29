d3a2abcfd0f24b2e7bcdc9a2f03b664097479da6
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-29 14:54:29 +0200

    Mise en place formulaire insertion en bootstrap

--- view/private/admin.insert.view.html.php
+++ view/private/admin.insert.view.html.php
@@ -21,6 +21,49 @@
     </header>
   </div>
   <!--Fin NAVBAR-->
+  <div id="content">
+        <h2 class="text-center mt-5">Ajoutez votre lieu</h2>
+        <div class="container ">
+            <div class="row  justify-content-center align-items-center">
+                <form method="POST" name="geo" action="">
+                    <div class="mb-3 mt-5">
+                     
+                        <input type=" text" name="nom" placeholder="Nom" class="form-control w-25  mx-auto border shadow-lg" id="nom" required>
+                    </div>
+                    <div class="mb-3 ">
+                        
+                        <input type="text" name="rue" class="form-control w-25 mx-auto shadow-lg" id="rue" placeholder="Adresse" required></input>
+                    </div>
+                    <div class="mb-3 ">
+                        
+                        <input type="number" name="codepostal" class="form-control w-25 mx-auto shadow-lg" id="codepostal" placeholder="Code Postal" required></input>
+                    </div>
+                    <div class="mb-3 ">
+                        
+                        <input type="number" name="telephone" class="form-control w-25 mx-auto shadow-lg" id="telephone" placeholder="Numéro téléphone" required></input>
+                    </div>
+                    <div class="mb-3 ">
+                        
+                        <input type="text" name="url" class="form-control w-25 mx-auto shadow-lg" id="url" placeholder="URL vers le site" required></input>
+                    </div>
+                    <div class="mb-3">
+                       
+                        <input type="number" placeholder="latitude" name="latitude" step="0.000000001" class="form-control w-25 mx-auto shadow-lg" id="latitude" required>
+                    </div>
+                    <div class="mb-3">
+                        
+                        <input type="number" name="longitude" placeholder="longitude" step="0.000000001" class="form-control w-25 mx-auto shadow-lg" id="longitude" required>
+                    </div>
+                    <div class="mb-3 text-center">
+                        <input type="submit" value="Insérer" class="btn btn-outline-primary">
+                    </div>
+                    <?php
+                    //var_dump($_POST);
+                    ?>
+                </form>
+            </div>
+        </div>
+    </div>
      <!--Lien JS Bootstrap-->
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
