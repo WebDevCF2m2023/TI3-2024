b230283c97a2cc3110118cabca8b6391bb44bf31
rammroumm2K <ramybrz00@gmail.com>
2024-04-29 10:49:23 +0200

    commit 3

--- model/localisationsModel.php
+++ model/localisationsModel.php
@@ -1,1 +1,102 @@
 <?php
+
+function getAllLocalisations(PDO $db): array|string
+{
+    $sql = "SELECT * FROM localisations ORDER BY id DESC;";
+    try {
+        $query = $db->query($sql);
+        if ($query->rowCount() === 0) return "Pas encore de datas";
+        $result = $query->fetchAll();
+        $query->closeCursor();
+        return $result;
+    } catch (Exception $e) {
+        return ['error' => $e->getMessage()];
+    }
+}
+
+
+function getOneLocalisations(PDO $db, int $id): array|string
+{
+    $sql = "SELECT * FROM localisations WHERE id = ?;";
+    $prepare = $db->prepare($sql);
+    $prepare->bindParam(1, $id, PDO::PARAM_INT);
+    try {
+        $prepare->execute();
+        if ($prepare->rowCount() === 0) return "Data non existante";
+        $result = $prepare->fetch();
+        $prepare->closeCursor();
+        return $result;
+    } catch (Exception $e) {
+        return ['error' => $e->getMessage()];
+    }
+}
+
+// mettre à jour une donnée
+function updateLocalisations(
+    PDO $db,
+    string $nom,
+    string $adresse,
+    int $codepostal,
+    string $ville,
+    float $latitude,
+    float $longitude,
+    int $id,
+
+
+): bool|string {
+    $prepare = $db->prepare("UPDATE localisations SET nom = ?, adresse = ?, codepostal = ?, ville = ? , latitude = ?, longitude = ? WHERE id = ?");
+    $prepare->bindParam(1, $nom);
+    $prepare->bindParam(2, $adresse);
+    $prepare->bindParam(3, $codepostal);
+    $prepare->bindParam(4, $ville);
+    $prepare->bindParam(5, $latitude);
+    $prepare->bindParam(6, $longitude);
+    $prepare->bindParam(7, $id, PDO::PARAM_INT);
+    try {
+        $prepare->execute();
+        return true;
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
+
+// ajoutez avec une requête préparée la nouvelle data
+function addLocalisations(
+    PDO $db,
+    string $nom,
+    string $adresse,
+    int $codepostal,
+    string $ville,
+    float $latitude,
+    float $longitude,
+    
+): bool|string {
+    $prepare = $db->prepare("INSERT INTO localisations (nom, adresse, codeposatl, ville, latitude, longitude) VALUES (?,?,?,?,?,?)");
+    $prepare->bindParam(1, $nom);
+    $prepare->bindParam(2, $adresse);
+    $prepare->bindParam(3, $codepostal);
+    $prepare->bindParam(4, $ville);
+    $prepare->bindParam(5, $latitude);
+    $prepare->bindParam(6, $longitude);
+    try {
+        $prepare->execute();
+        return true;
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
+
+// supprimer une donnée
+
+function deleteLocalisations(PDO $db, int $id): bool|string
+{
+    $prepare = $db->prepare("DELETE FROM localisations WHERE id = ?");
+    $prepare->bindParam(1, $id, PDO::PARAM_INT);
+    try {
+        $prepare->execute();
+        if ($prepare->rowCount() === 0) return "Data non existante";
+        return true;
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,1 +1,33 @@
 <?php
+
+session_start();
+
+
+require_once '../config.php';
+
+require_once "../model/localisationsModel.php";
+require_once "../model/utilisateursModel.php";
+
+
+try {
+    $connect = new PDO(
+        DB_DRIVER . ":host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET,
+        DB_LOGIN,
+        DB_MDP,
+        [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+    );
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
+
+if (isset($_SESSION['myID']) && $_SESSION['myID'] == session_id()) {
+
+    require_once "../controller/privateController.php";
+} else {
+
+
+    require_once "../controller/publicController.php";
+}
+
+$connect = null;
