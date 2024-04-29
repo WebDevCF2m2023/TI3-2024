a9528702a1069daacae9e5f71a684a7afd3cb07e
PommeDeuxTerre <lemonniergregory2@gmail.com>
2024-04-29 12:37:14 +0200

    home bootstrap table

--- public/js/main.js
+++ public/js/main.js
@@ -13,11 +13,17 @@ function handle_res(locations){
         alert(locations.error);
         return;
     }
+    //add markers to map
     locations.forEach((location)=>{
         add_marker_location(location);
     })
     const group = new L.featureGroup(markers);
     map.fitBounds(group.getBounds());
+    //add locations to list
+    const tbody = document.querySelector("#locations-table tbody");
+    locations.forEach((location)=>{
+        tbody_add_location(tbody, location);
+    })
 }
 
 function add_marker_location(location){
@@ -25,4 +31,37 @@ function add_marker_location(location){
     marker.bindPopup(`<h3>${location.nom}</h3><p>${location.rue} ${location.codepostal} Bruxelles</p><p>tel: ${location.telephone}</p><a href=${location.url}">Voir le site</a>`);
     markers_hashmap[location.id] = marker;
     markers.push(marker);
+}
+
+function tbody_add_location(tbody, location){
+    const tr = document.createElement("tr");
+    //name
+    const td1 = document.createElement("td");
+    td1.textContent = location.nom;
+    //adresse
+    const td2 = document.createElement("td");
+    td2.textContent = `${location.rue} ${location.codepostal} Bruxelles`;
+    //telephone
+    const td3 = document.createElement("td");
+    td3.textContent = location.telephone;
+    //url
+    const td4 = document.createElement("td");
+    const a4 = document.createElement("a");
+    a4.href = location.url;
+    a4.textContent = "Voir le site";
+    td4.insertAdjacentElement("afterbegin", a4);
+    //icon
+    const td5 = document.createElement("td");
+    td5.classList.add("text-center");
+    const icon_i = document.createElement("i");
+    icon_i.classList.add("fa", "fa-eye", "text-primary")
+    td5.insertAdjacentElement("afterbegin", icon_i);
+    //insert tds in tr
+    tr.insertAdjacentElement("beforeend", td1);
+    tr.insertAdjacentElement("beforeend", td2);
+    tr.insertAdjacentElement("beforeend", td3);
+    tr.insertAdjacentElement("beforeend", td4);
+    tr.insertAdjacentElement("beforeend", td5);
+    //insert the tr
+    tbody.insertAdjacentElement("afterbegin", tr);
 }
--- view/public/home.php
+++ view/public/home.php
@@ -7,9 +7,36 @@
     <link rel="stylesheet" href="css/style.css">
     <!-- leaflet -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- bootstrap table -->
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.22.4/dist/bootstrap-table.min.css">
 </head>
 <body>
+    <h1>Carte interactive</h1>
+    <h2>Liste théatres à Bruxelles</h2>
+    <p id="login-link"><a href="./?p=login">Connexion à l'administration</a></p>
     <div id="map"></div>
+    <!-- table -->
+    <div class="container-lg">
+        <div class="table-responsive">
+            <table class="table table-striped" data-click-to-select="true" data-toggle="table" data-show-columns="true" data-pagination="true" id="locations-table">
+                <thead>
+                    <tr>
+                        <th data-sortable="true">Nom</th>
+                        <th>Adresse</th>
+                        <th>Telephone</th>
+                        <th>Url</th>
+                        <th>Voir le lieux sur la map</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
     <!-- leaflet -->
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     <!-- local script -->
