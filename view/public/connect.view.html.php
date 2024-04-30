7afedf820b042f18478637f688d81be65da79d20
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-30 08:58:12 +0200

    Ajout dans le head lien fonts et ajout d'un id sur mon h3

--- view/public/connect.view.html.php
+++ view/public/connect.view.html.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
     <!--Lien CSS Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
 
 <div id="content">
-        <h3 class="text-center mt-5">Connexion à notre administration</h3>
+        <h3 class="text-center mt-5" id="titleConnexion">Connexion à notre administration</h3>
         <?php if (isset($error)) : ?>
             <h4 id="alert" class="text-center mt-4"><?= $error ?></h4>
         <?php endif ?>
@@ -21,7 +24,7 @@
                     <img src="img/forgotPassword.jpg" alt="">
                 </div>
                 <div class="col-md-10 mx-auto col-lg-5">
-                    <form method="POST" name="connexion" action="" class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+                    <form method="POST" name="connexion" action="" class="p-4 p-md-5 border rounded-3" id="loginForm">
                         <div class="form-floating mb-3">
                             <input type="text" class="form-control" id="floatingInput" placeholder="Votre login" name="username">
                             <label for="username">Login</label>
