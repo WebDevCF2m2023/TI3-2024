b1660544f8d1cce63c1f8b1a8b50c5568d93e647
leerlandais <leerlandais@gmail.com>
2024-05-02 18:13:03 +0200

    dbConnection and JSONmaker added to controllers

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,1 +1,21 @@
 <?php
+
+if (isset($_GET["login"])) {
+    //  if -------LOGIN POSTS HERE
+        $title = "Connexion d'utilisateur";
+        include "../view/public/login.view.php";
+    die();
+}
+
+
+$cinemas = getAllCinemas($db);  // Avant d'afficher la page, prépare les informations nécessaires.
+    if ($cinemas === false) {
+        $errorMessage = "Impossible d'obtenir les informations de la base de données en raison d'une erreur dans le SQL.";
+    }else if (is_string($cinemas)) {
+        $errorMessage = $e;
+    }
+
+    $title = 'Bienvenue dans ma nouvelle version de TI3';
+    $cineCount = count($cinemas);
+    include "../view/public/pubhome.view.php";
+die();
