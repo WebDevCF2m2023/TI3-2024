b899e94a8033675edd5436e3bda0b555e7bf3efe
sebastien2506 <jestrovic_8@msn.com>
2024-05-06 13:02:44 +0200

    modif bug

--- view/private/admin.homepage.html.php
+++ view/private/admin.homepage.html.php
@@ -157,26 +157,21 @@
             <div class="container-fluid pt-4 px-4">
                 <div class="bg-secondary text-center rounded p-4">
                     <div class="d-flex align-items-center justify-content-between mb-1">
-                        <h6 class="mb-0">Recent data</h6>
                         
                     </div>
-                    <h2 class="mb-5">Administration de nos datas</h2>
-                    <ul class="list-unstyled mt-5">
-               <li class=""><a href="./">Accueil Admin</a></li>
-               <li><a href="?insert">Ajouter une data</a></li>
-               <li><a href="?disconnect">Déconnexion</a></li>
-           </ul>
+                    <h2 class="mb-2">Administration de nos datas</h2>
+
         <?php if(isset($_GET['co'])): ?>
-        <h2 id="title" class=" display-4 text-success fst-italic animate__animated animate__zoomIn  ">Bienvenue <?=$_SESSION['login']?></h2>
+            <h2 id="title" class=" mb-3 display-4 text-success fst-italic animate__animated animate__zoomIn  ">Bienvenue <?=$_SESSION['login']?></h2>
         <?php endif ?>
         <?php if(isset($_GET['zut'])): ?>
-            <h2 id="insert" class="display-5 text-info animate__animated animate__zoomIn animate__delay-1s  ">Insertion réussie</h2>
+            <h2 id="insert" class="mb-3 display-5 text-info animate__animated animate__zoomIn animate__delay-1s  ">Insertion réussie</h2>
         <?php endif ?>
         <?php if(isset($_GET['up'])): ?>
-          <h2 id="up" class=" display-5 text-info animate__animated animate__zoomIn animate__delay-1s ">Update réussie</h2>
+          <h2 id="up" class="mb-3 display-5 text-info animate__animated animate__zoomIn animate__delay-1s ">Update réussie</h2>
         <?php endif ?>
         <?php if(isset($_GET['ok'])): ?>
-          <h2 id="del" class=" display-5 text-info animate__animated animate__zoomIn animate__delay-1s ">Delete réussie</h2>
+          <h2 id="del" class="mb-3 display-5 text-info animate__animated animate__zoomIn animate__delay-1s ">Delete réussie</h2>
         <?php endif ?>
         <?php  if(isset($message)): ?>
                 <h3><?=$message?></h3>
@@ -195,6 +190,20 @@
                         data-show-columns="true"
                         data-show-columns-toggle-all="true"
                         data-resizable="true" class="table text-start align-middle table-bordered table-hover mb-0">
+
+
+
+                        <div class="mb-2">
+                        <a href="?insert">
+                        <button type="button" class="btn btn-success">
+                        <svg style="position:relative; top: -1px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-database-fill-add me-1" viewBox="0 0 16 16">
+                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0M8 1c-1.573 0-3.022.289-4.096.777C2.875 2.245 2 2.993 2 4s.875 1.755 1.904 2.223C4.978 6.711 6.427 7 8 7s3.022-.289 4.096-.777C13.125 5.755 14 5.007 14 4s-.875-1.755-1.904-2.223C11.022 1.289 9.573 1 8 1"></path>
+                        <path d="M2 7v-.839c.457.432 1.004.751 1.49.972C4.722 7.693 6.318 8 8 8s3.278-.307 4.51-.867c.486-.22 1.033-.54 1.49-.972V7c0 .424-.155.802-.411 1.133a4.51 4.51 0 0 0-4.815 1.843A12 12 0 0 1 8 10c-1.573 0-3.022-.289-4.096-.777C2.875 8.755 2 8.007 2 7m6.257 3.998L8 11c-1.682 0-3.278-.307-4.51-.867-.486-.22-1.033-.54-1.49-.972V10c0 1.007.875 1.755 1.904 2.223C4.978 12.711 6.427 13 8 13h.027a4.55 4.55 0 0 1 .23-2.002m-.002 3L8 14c-1.682 0-3.278-.307-4.51-.867-.486-.22-1.033-.54-1.49-.972V13c0 1.007.875 1.755 1.904 2.223C4.978 15.711 6.427 16 8 16c.536 0 1.058-.034 1.555-.097a4.5 4.5 0 0 1-1.3-1.905"></path>
+                        </svg>
+                        Ajouter Location
+                    </button>
+                </a>
+            </div>
                             <thead>
                                 <tr>
                                   <th data-checkbox="true" data-field="state"></th>
