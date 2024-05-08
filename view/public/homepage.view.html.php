5ba7db7d20ff666e13ace8ac64f6b883d13fc5fa
Anthoonyyy <Anthony212007@hotmail.com>
2024-05-08 02:04:24 +0200

    ajout fond gris

--- view/public/homepage.view.html.php
+++ view/public/homepage.view.html.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <!--Lien CSS Leaflet-->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-     crossorigin=""/>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     <title>Accueil</title>
+
 </head>
-<body>
-<div id="content">
+
+<body class="fondGris">
+    <div id="content">
         <h1 class="titreHomepage">Carte interactive</h1>
-        <h3 class="titreHomepage">Théâtre à Bruxelles</h3>
-         <a href="?connect" id="btnHomepage">Connexion à l'administration</a> 
+        <h3 class="titreHomepage">Théâtres à Bruxelles</h3>
+
+        <div>
+            <a href="?connect"> <button class="btn"><i class="animation"></i>Connexion à l'administration<i class="animation"></i>
+                </button></a>
+        </div>
+        </a>
+        <!-- <a href="?connect" id="btnHomepage">Connexion à l'administration</a>-->
         <?php
         /*
         // datas est une chaine de caractère : erreur SQL ! 
@@ -55,18 +62,17 @@
             <div id="map"></div>
             <div id="liste">
                 <h2 class="titreHomepage">Listes des théâtres</h2>
-                <p  class="titreHomepage">Cliquez sur un élément dans la liste ci-dessous pour le situer sur la carte</p>
+                <p class="titreHomepage">Cliquez sur un élément dans la liste ci-dessous pour le situer sur la carte</p>
                 <hr>
             </div>
         </div>
 
     </div>
 
-<!--JS de Leaflet-->
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-     crossorigin=""></script>
-     <!--Mon JS-->
-     <script src="js/carteJSON.js"></script>
+    <!--JS de Leaflet-->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <!--Mon JS-->
+    <script src="js/carteJSON.js"></script>
 </body>
+
 </html>
